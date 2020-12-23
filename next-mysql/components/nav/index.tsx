98a42d3f7cf7448cd451f9aa5a1c42e2aef1691d
BrandeenoLee <brandonladcock@gmail.com>
2020-12-23 16:30:42 -0600

    Created Navbar.

--- next-mysql/components/nav/index.tsx
+++ next-mysql/components/nav/index.tsx
@@ -1,17 +1,35 @@
 import Link from 'next/link'
 import Container from '@/components/container'
 import ButtonLink from '@/components/button-link'
+import styles from '../nav/navbar.module.scss'
+
 
 export default function Nav({ title = 'Entries' }) {
   return (
     <Container className="py-4">
-      <nav>
-        <div className="flex justify-between items-center">
-          <Link href="/">
-            <a className="font-bold text-3xl">{title}</a>
-          </Link>
-          <ButtonLink href="/new">New Entry</ButtonLink>
-        </div>
+      <nav className={styles.nav}>
+      <ul>
+      <li>
+        <Link
+          href={{
+            pathname: '/about',
+            query: { name: 'test' },
+          }}
+        >
+          <a>Hours</a>
+        </Link>
+      </li>
+      <li>
+        <Link
+          href={{
+            pathname: '..../pages/hours.ts'
+          }}
+        >
+          <a></a>
+        </Link>
+      </li>
+    </ul>
+          <ButtonLink href="/new">Add Volunteer</ButtonLink>
       </nav>
     </Container>
   )
