49e7e6c86d62929bc453f42ad0709cee50c1f57e
BrandeenoLee <brandonladcock@gmail.com>
2020-12-28 17:15:22 -0600

    Created email form and Date Picker.

--- pages/hours.tsx
+++ pages/hours.tsx
@@ -1,14 +1,46 @@
+import React, { useState } from 'react';
 import Nav from '@/components/nav'
 import Container from '@/components/container'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 
 export default function Hours() {
-  return (
-    <>
-      <Nav activePage="hours" />
-      <Container className="w-full lg:w-2/4">
-          <p>hours</p>
-      </Container>
-    </>
+      const [email, setEmail] = useState('')
+      const [startDate, setStartDate] = useState(new Date());
+      const [endDate, setEndDate] = useState(new Date());
+  
+
+
+    return (
+      <>
+        <Nav activePage="hours" />
+          <Container className="w-full lg:w-2/4">
+
+              <form>
+                <p>Enter Email</p>
+              <input
+              type='text'
+                />
+              </form>
+              <DatePicker 
+                  selected={startDate} 
+                  onChange={date => setStartDate(date)}
+                  selectsStart
+                  startDate={startDate}
+                  endDate={endDate}
+                  isClearable
+                  />
+              <DatePicker 
+                  selected={endDate} 
+                  onChange={date => setEndDate(date)} 
+                  selectsEnd 
+                  startDate={startDate} 
+                  endDate ={endDate} 
+                  minDate={startDate} 
+                  isClearable
+                  />
+          </Container>
+      </>
   )
 }
