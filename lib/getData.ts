3e5ecefb324b8eccb724a99038755412c77144f6
Ashley <asmith@evernote.com>
2020-12-30 14:15:56 -0600

    Show todays scheduled volunteers on home and add api methods

--- lib/getData.ts
+++ lib/getData.ts
@@ -7,19 +7,9 @@ const base = new Airtable({
 const volunteersTable = base(process.env.NEXT_PUBLIC_AIRTABLE_TABLE_NAME_VOLUNTEERS);
 const shiftsTable = base(process.env.NEXT_PUBLIC_AIRTABLE_TABLE_NAME_SHIFTS);
 
-
-const getMinifiedData = volunteers => {
-return volunteers.map(v => minify(v));
+const getMinifiedData = data => {
+  return data.map(d => d.fields);
 };
-
-// gets the data we want and puts it into variables
-const minify = data => data.fields;
-// {
-//     return {
-//         id: volunteer.id,
-//         fields: volunteer.fields,
-//     };
-// };
 
 export default async function getData(table: 'volunteers' | 'shifts', options = {}) {
     if (table === 'volunteers') {
@@ -27,18 +17,14 @@ export default async function getData(table: 'volunteers' | 'shifts', options = 
         return volunteers;
     }
     return getShifts(options);
-    // return shifts;
 }
 
 const getVolunteers = async (options = {}) => {
   const volunteers = await volunteersTable.select(options).all();
   return getMinifiedData(volunteers)
-//   console.log('vol result', minifiedVolunteerData.map(d => d.fields))
-//   return minifiedVolunteerData.map(d => d.fields);
 }
 
-const getShifts = async (options = {}) => { // { email: string; from: Date; to: Date; }
-    const shifts = await shiftsTable.select(options).all();
-    // ({ view: options.email, filterByFormula: `IS_BEFORE([date1], [date2])` }).all();
-    return getMinifiedData(shifts);
+const getShifts = async (options = {}) => {
+  const shifts = await shiftsTable.select(options).all();
+  return getMinifiedData(shifts);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,115 +1,74 @@
 import Container from "@/components/container";
 import Nav from "@/components/nav";
+import { filterToToday } from "@/lib/airtableFormulas";
 import getData from "@/lib/getData";
 import React, { useState, useEffect } from "react";
 
 export default function IndexPage() {
-  const [error, setError] = useState(null);
-  const [shifts, setShifts] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
-  const today = new Date().toLocaleDateString('en-us');
-  // Note: the empty deps array [] means
-  // this useEffect will run once
-  // similar to componentDidMount()
+  const [todaysShifts, setTodaysShifts] = useState([]);
+  const today = new Date().toLocaleDateString();
+
   useEffect(() => {
-    getData('shifts', { filterByFormula: `{date}=${today}` })
-      .then(res => {
-        console.log('___shifts', res)
-        setShifts(res);
+    getData('shifts', { filterByFormula: filterToToday(), fields: ['email', 'shift', 'checkedin', 'checkedout'] })
+      .then(shifts => {
+        console.log('___shifts', shifts)
+        setTodaysShifts(shifts);
       });
-      // .then(
-      //   (result) => {
-      //     setIsLoaded(true);
-      //     // setItems(result);
-      //   },
-      //   // Note: it's important to handle errors here
-      //   // instead of a catch() block so that we don't swallow
-      //   // exceptions from actual bugs in components.
-      //   (error) => {
-      //     setIsLoaded(true);
-      //     setError(error);
-      //   }
-      // )
   }, [])
 
+  const getShiftText = (shift: 1 | 2 | 3 | 4): string => {
+    let str = '';
+    switch(shift) {
+      case 1:
+        str = '8AM - 11AM'
+        break;
+      case 2:
+        str = '11AM - 2PM'
+        break;
+      case 3:
+        str = '2PM - 5PM'
+        break;
+      case 4:
+        str = '5PM - 8PM'
+        break;
+      default:
+        str = 'Unknown'
+    }
+    return str;
+  }
+
+  /**
+   * TODO:
+   * - Get volunteer name instead of email
+   */
 
   return (
     <div>
       <Nav activePage='home'/>
       <Container>
-        <h1>Today's Volunteers</h1>
-        {/* {shifts} */}
-        {/* {volunteers.map(v => <div>{v}</div>)} */}
+        <h1>Today's Volunteers <span>({today})</span></h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Volunteer</th>
+              <th>Shift</th>
+              <th>Check In/Out</th>
+            </tr>
+          </thead>
+          <tbody>
+            {todaysShifts.map(({ email, shift, checkedin, checkedout }) => (
+              <tr key={email}>
+                <td>{email}</td>
+                <td>{getShiftText(shift)}</td>
+                <td>
+                  <button>{checkedin && !checkedout ? 'Clock Out' : 'Clock In'}</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </Container>
     </div>
     );
 }
 
-
-
-// import Nav from '@/components/nav'
-// import Container from '@/components/container'
-// // import Entries from '@/components/entries'
-
-// import { useEntries } from '@/lib/swr-hooks'
-// import getData from '@/lib/getData'
-// import { useEffect, useState } from 'react';
-
-// // const getVolunteers = async () => {
-// //   return getData('volunteers');
-// // }
-
-// // const getShifts = async () => {
-// //   return getData('shifts');
-// // }
-
-// export default async function IndexPage() {
-//   // const volunteers = await getData('volunteers');
-//   // const shifts = await getData('shifts');//, { filterByFunction: 'email==='});
-//   // console.log('___volunteers', volunteers);
-//   // console.log('___shifts', shifts);
-
-//   const [volunteers, setVolunteers] = useState([]);
-//   const [shifts, setShifts] = useState([]);
-//   // const [data, setData] = useState({ hits: [] });
-//   // const [query, setQuery] = useState('react');
-
-//   useEffect(() => {
-//     let ignore = false;
-
-//     async function fetchData(): void {
-//       const result = await getData('volunteers');
-//       console.log('___RESULT', result)
-//       if (!ignore) setVolunteers(result);
-//     }
-
-//     fetchData();
-//     return () => { ignore = true; }
-//   }, []);
-
-//   // return (
-//   //   <>
-//   //     <input value={query} onChange={e => setQuery(e.target.value)} />
-//   //     <ul>
-//   //       {data.hits.map(item => (
-//   //         <li key={item.objectID}>
-//   //           <a href={item.url}>{item.title}</a>
-//   //         </li>
-//   //       ))}
-//   //     </ul>
-//   //   </>
-//   // );
-//   // }, [])
-//   // console.log('process.env', process.env)
-//   // console.log('___volunteers.fields', volunteers.fields);
-//   return (
-//     <div>
-//       <Nav activePage='home'/>
-//       <Container>
-//         <h1>Today's Volunteers</h1>
-//         {/* {volunteers.map(v => <div>{v}</div>)} */}
-//       </Container>
-//     </div>
-//   )
-// }
