2a436290c863a3b93ecb4390d9c74e4b1a783f45
BrandeenoLee <brandonladcock@gmail.com>
2021-01-01 14:58:42 -0600

    Deleted old API Page.

--- pages/api/edit-entry.ts
+++ pages/api/edit-entry.ts
@@ -1,31 +1,31 @@
-import { NextApiHandler } from 'next'
-import Filter from 'bad-words'
-import { query } from '../../lib/db'
+// import { NextApiHandler } from 'next'
+// import Filter from 'bad-words'
+// import { query } from '../../lib/db'
 
-const filter = new Filter()
+// const filter = new Filter()
 
-const handler: NextApiHandler = async (req, res) => {
-  const { id, title, content } = req.body
-  try {
-    if (!id || !title || !content) {
-      return res
-        .status(400)
-        .json({ message: '`id`,`title`, and `content` are all required' })
-    }
+// const handler: NextApiHandler = async (req, res) => {
+//   const { id, title, content } = req.body
+//   try {
+//     if (!id || !title || !content) {
+//       return res
+//         .status(400)
+//         .json({ message: '`id`,`title`, and `content` are all required' })
+//     }
 
-    const results = await query(
-      `
-      UPDATE entries
-      SET title = ?, content = ?
-      WHERE id = ?
-      `,
-      [filter.clean(title), filter.clean(content), id]
-    )
+//     const results = await query(
+//       `
+//       UPDATE entries
+//       SET title = ?, content = ?
+//       WHERE id = ?
+//       `,
+//       [filter.clean(title), filter.clean(content), id]
+//     )
 
-    return res.json(results)
-  } catch (e) {
-    res.status(500).json({ message: e.message })
-  }
-}
+//     return res.json(results)
+//   } catch (e) {
+//     res.status(500).json({ message: e.message })
+//   }
+// }
 
-export default handler
+// export default handler
